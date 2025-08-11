aa2f3adbc9a0799b0df3d748daed4b0f182a4f5a
DELL <you@example.com>
2025-08-11 19:57:24 +0530

    Add bracelates image

--- src/components/wishlist.jsx
+++ src/components/wishlist.jsx
@@ -81,7 +81,7 @@ function wishlist() {
  <div className='flex justify-center items-center mt-10 mb-10 space-x-6'>
              <Link to='/card/rings'><img className=' w-72 h-85 cursor-pointer hover:scale-102 duration-500 ' src={ring} alt="" />  <p className='flex  relative bottom-9 left-25 text-amber-50'>Rings</p></Link> 
               <Link to='/card/necklace'><img className=' w-72 h-85 cursor-pointer hover:scale-102 duration-500 ' src={necklace} alt="" /> <p className='flex relative bottom-9 left-25 text-amber-50'>Necklaces</p></Link> 
-             <Link to='/card/bracelet'> <img className=' w-72 h-85 cursor-pointer hover:scale-102 duration-500 ' src={bracelates} alt="" /> <p className='flex relative bottom-9 left-25 text-amber-50'>Bracelets</p></Link> 
+             <Link to='/card/bracelet'><img className=' w-72 h-85 cursor-pointer hover:scale-102 duration-500 ' src="/assets/bracelates.jpg" alt="Bracelet" /> <p className='flex relative bottom-9 left-25 text-amber-50'>Bracelets</p></Link> 
               <Link to='/card/earring'><img className=' w-72 h-85 cursor-pointer hover:scale-102 duration-500 ' src={earring} alt="" /> <p className=' w-10 flex relative bottom-9 left-25 text-amber-50'>Earrings</p></Link> 
         </div>
   </div>}
