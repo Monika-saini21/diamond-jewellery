bd036a38cab0d8d4daca872f320a3fc4b8275803
DELL <you@example.com>
2025-08-11 19:52:30 +0530

    Fix image file name for Netlify

--- src/components/category.jsx
+++ src/components/category.jsx
@@ -2,7 +2,7 @@
 import ring from '../assets/ring.jpg'
 import earring from '../assets/earring.jpg'
 import necklace from '../assets/necklace.jpg'
-import bracelates from '../assets/bracelates.jpg'
+
 import { Link,  } from 'react-router-dom'
 
 
@@ -11,7 +11,7 @@ function Category(){
         <div className='flex justify-center  items-center mt-19 mb-10 space-x-6'>
            <Link to={"/card/rings"}><img className=' w-68 h-85 cursor-pointer hover:scale-102 duration-500 ' src={ring} alt="" />  <p className='flex  relative bottom-9 left-25 text-amber-50'>Rings</p></Link>
             <Link  to={"/card/necklace"}><img className=' w-68 h-85 cursor-pointer hover:scale-102 duration-500 ' src={necklace} alt="" /> <p className='flex relative bottom-9 left-25 text-amber-50'>Necklaces</p></Link>
-           <Link to={"/card/bracelet"}><img className=' w-68 h-85 cursor-pointer hover:scale-102 duration-500 ' src={bracelates} alt="" /> <p className='flex relative bottom-9 left-25 text-amber-50'>Bracelets</p></Link>
+           <Link to={"/card/bracelet"}><img className=' w-68 h-85 cursor-pointer hover:scale-102 duration-500 '  src="/assets/bracelates.jpg" alt="Bracelet" /> <p className='flex relative bottom-9 left-25 text-amber-50'>Bracelets</p></Link>
             <Link to={"/card/earring"}><img className=' w-68 h-85 cursor-pointer hover:scale-102 duration-500 ' src={earring} alt="" /> <p className=' w-10 flex relative bottom-9 left-25 text-amber-50'>Earrings</p></Link>
     
         </div>
